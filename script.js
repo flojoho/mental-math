a048422c0ce6c8b2058d8fbe8cb769c53f187181
flojoho <hofkas.flo@gmx.at>
2022-09-29 22:45:07 +0200

    add number grid and answer checking

--- script.js
+++ script.js
@@ -1,16 +1,53 @@
 
 const problemDiv = document.getElementById('problemDiv');
+const answerDiv = document.getElementById('answerDiv');
 
 const randomInt = () => {
   return Math.floor(Math.random() * 11);
 }
 
-const showRandomProblem = () => {
-  problemDiv.textContent = `${randomInt()}×${randomInt()}`;
+let number1;
+let number2;
+let answer = '0';
+
+const generateProblem = () => {
+  number1 = randomInt();
+  number2 = randomInt()
+  problemDiv.textContent = `${number1}×${number2}`;
+}
+generateProblem();
+
+const setAnswer = newAnswer => {
+  answer = String(newAnswer);
+  answerDiv.textContent = `= ${ newAnswer }`;
 }
 
-showRandomProblem();
+const enterNumber = number => {
+  if(parseInt(answer) === 0) {
+    setAnswer(number);
+  } else {
+    setAnswer(`${ answer }${ number }`);
+  }
+}
 
-document.addEventListener('keydown', e => {
-  showRandomProblem();
-});
+const removeNumber = () => {
+  if(answer === '0') return;
+  const newAnswer = answer.slice(0, -1);
+  if(newAnswer === '') {
+    setAnswer('0');
+  } else {
+    setAnswer(newAnswer);
+  }
+}
+
+const checkAnswer = () => {
+  const isRight = parseInt(answer) === number1 * number2;
+
+  setAnswer('0');
+
+  answerDiv.className = '';
+  answerDiv.classList.add(isRight ? 'right-answer' : 'wrong-answer');
+  setTimeout(() => answerDiv.classList.add('fade-out'), 1);
+
+  if(isRight) generateProblem();
+}
